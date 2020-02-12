dafd0130210fd6d936f0a9d70db28cc0a1dd71cb
Rodrigo Bossini <professorbossini@gmail.com>
2020-02-12 16:29:58 -0300

    Initial commit

---
+++ app/src/main/java/com/example/fatec_ipi_tarde_introducao_recursos/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.fatec_ipi_tarde_introducao_recursos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
